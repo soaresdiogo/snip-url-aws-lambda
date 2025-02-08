d4d4cc0a5dc87f470ad8b5ca19ad9f4408a4bff4
Diogo Soares <diogoschsoares@gmail.com>
2025-02-08 15:00:27 +0000

    feat: Initial Commit

---
+++ src/main/java/dev/diogosoares/Main.java
@@ -0,0 +1,14 @@
+package dev.diogosoares;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+
+import java.util.Map;
+
+public class Main implements RequestHandler<Map<String, Object>, Map<String, String>>{
+
+    @Override
+    public Map<String, String> handleRequest(Map<String, Object> stringObjectMap, Context context) {
+        return null;
+    }
+}
