96c9f3649887a2791bdee398cfdf48f310575c15
Diogo Soares <diogoschsoares@gmail.com>
2025-02-09 10:48:38 +0000

    feat: Added a handle function to generate my sniped url

--- src/main/java/dev/diogosoares/Main.java
+++ src/main/java/dev/diogosoares/Main.java
@@ -2,13 +2,36 @@ package dev.diogosoares;
 
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
+import java.util.HashMap;
 import java.util.Map;
+import java.util.UUID;
 
 public class Main implements RequestHandler<Map<String, Object>, Map<String, String>>{
 
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
     @Override
-    public Map<String, String> handleRequest(Map<String, Object> stringObjectMap, Context context) {
-        return null;
+    public Map<String, String> handleRequest(Map<String, Object> input, Context context) {
+        String body =  (String) input.get("body");
+
+        Map<String, String> bodyMap;
+        try{
+            bodyMap = objectMapper.readValue(body, Map.class);
+        } catch (JsonProcessingException jsonProcessingException){
+            throw new RuntimeException("Error parsing JSON body: " + jsonProcessingException.getMessage(), jsonProcessingException);
+        }
+
+        String originalUrl = bodyMap.get("originalUrl");
+        String expirationTime = bodyMap.get("expirationTime");
+
+        String shortUrlCode = UUID.randomUUID().toString().substring(0, 8);
+
+        Map<String, String> response = new HashMap<>();
+        response.put("code", shortUrlCode);
+
+        return response;
     }
 }
